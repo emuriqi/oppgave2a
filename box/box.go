fa086db91cadceca3f2ec67e24070f39dc0f97ce
Elion <oppgave2a@email.com>
2022-01-28 14:42:21 +0100

    oppgave2a kode

---
+++ box/box.go
@@ -0,0 +1,8 @@
+package box
+
+import "rsc.io/quote"
+
+func TestQuote() string {
+	return quote.Glass() string {
+	return quote.Glass() + "\n" + quote.Go() + "\n" + quote.Hello() + "\n" + quote.OPT();
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main 
+
+import (
+	"fmt"
+	"github.com/emuriqi/modul1/oppgave2a"
+)
+func main() {
+	fmt.println(pakke TestQuote())
+}
